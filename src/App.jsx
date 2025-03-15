f0d1c8992349da2ea4409ad3cb38a9d9b4a04468
biswas lohani <lohanibiswas1@gmail.com>
2025-03-15 17:53:20 +0545

    the end

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import Product from './Pages/Product'
 import Cart from './Pages/Cart'
 import LoginSignup from './Pages/LoginSignup'
 
+import men_banner from './Components/Assests/banner_mens.png'
+import women_banner from './Components/Assests/banner_women.png'
+import kid_banner from './Components/Assests/banner_kids.png'
+import Footer from './Components/Footer/Footer'
+
+
+
 
 const App = () => {
   return (
@@ -15,15 +22,19 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="mens"/>} />
-        <Route path='/womens' element={<ShopCategory category="mens"/>}/>
-        <Route path='/kids' element={<ShopCategory category="mens"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>} />
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kids"/>}/>
         <Route path='/product' element={<Product/>}/>
         <Route path=':productId' element={<Product/>}/>
         <Route path='/cart' element={<Cart/>}/>
+        
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
+      <Footer/>
+      
       </BrowserRouter>
+    
     </div>
   )
 }
